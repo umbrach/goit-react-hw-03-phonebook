110a97eeaa49513b628a0184f4002e792ab273d6
Denys Barabanov <umbrach415@gmail.com>
2022-11-25 14:45:50 +0200

    Add contacts to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,26 @@ import ContactList from './ContactList/ContactList';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parceContacts = JSON.parse(contacts);
+
+    this.setState({ contacts: parceContacts });
+
+    console.log(parceContacts);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('field updated')
+
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (name, number) => {
     const contact = {
